b5a081523bd254072150b072ac269ea91eda5d45
jessebibee <jesse.bibee@gmail.com>
2014-04-11 17:30:35 -0700

    clean, uglify, concat grunt build script

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,44 @@ module.exports = function (grunt) {
                 autoWatch: false,
                 singleRun: true
             }
+        },
+        jshint: {
+            files: ['Gruntfile.js', 'src/js/**/*.js', 'test/**/*.js']
+        },
+        clean: {
+            all: {
+                src: ['build/**/*.*']
+            }
+        },
+        uglify: {
+            all: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['src/js/**/*.js'],
+                        dest: 'build/',
+                        ext: '.min.js'
+                    }
+                ]
+            }
+        },
+        concat: {
+            options: {
+                seperator: ';'
+            },
+            dist: {
+                src: ['build/src/**/*.min.js'],
+                dest: 'build/app.js'
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     grunt.registerTask('test:unit', ['karma:unit']);
+    grunt.registerTask('default', ['clean', 'uglify', 'concat']);
 };
